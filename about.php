b8f3e8daff98ec855afd1fdd39cc0a2536c864ff
lhudymuel <lhudymuel@gmail.com>
2023-11-08 10:13:55 +0800

    aasmnhscode

---
+++ about.php
@@ -0,0 +1,41 @@
+   <!-- /.row -->
+          
+                     
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+                               aaaaaaa
+
+                                  
+
+                                     
+
+                                   <h1>  MISSION</h1>
+                                   aaaaaa
+
+                                    
+
+                                     
+
+                                   <h1> GOALS</h1>
+                                   <ul>
+                                   <li>aaaaaaa</li>
+                                   </ul>
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
---
+++ home.php
@@ -0,0 +1,383 @@
+<html>
+    <head>
+        <title>
+            Assmnhs Emrollment
+        </title>
+
+        <link rel="stylesheet" href="css/indexstyle.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/servicestyle.css">
+        <link rel="stylesheet" href="css/scroll.css">
+        <link href="css/modern.css" rel="stylesheet">
+       
+
+        <meta name="viewport" content="width =device-width, initial-scale=1.p">
+        <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+         <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+ <!-- Custom Fonts -->
+        <link href="font/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<!-- <link href="<?php echo web_root; ?>font/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
+ <!-- DataTables CSS -->
+        <link href="css/dataTables.bootstrap.css" rel="stylesheet">
+
+  <!-- datetime picker CSS -->
+        <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+
+       
+        <link href="css/costum.css" rel="stylesheet">
+
+        <link href="css/ekko-lightbox.css" rel="stylesheet">
+
+<style type="text/css">
+.p {
+
+    color: white;
+     margin-bottom: 0;
+    margin-top: 0;
+    /*padding: 0;*/
+    /*float: right;*/
+    list-style: none;
+  }
+  
+  .p > a { 
+    color: white;
+    /*text-align: center;*/
+    margin-bottom: 0;
+    margin: 0;
+    padding: 0;
+    text-decoration:none;
+    background-color:  #0000FF;
+  }
+  .p > a:hover ,
+  .p > a:focus {
+     color: black; 
+     text-decoration:none;
+     background-color: #2d52f2;
+  }
+  
+  
+   
+  .title-logo  {
+      color:black;
+      text-decoration:none;
+      font-size: 42px;
+      font-family: "broadway";
+      /*font-style: bold;*/
+      padding: 0;
+      margin: 0;
+      top: 0;
+    
+    }
+  .title-logo:hover {
+    color: blue; 
+    text-decoration:none; 
+  }
+  .carttxtactive {
+    color: red;
+    font-style: bold;
+    box-shadow: red;
+  
+  }
+  .carttxtactive:hover {
+     color: white;
+  }
+  
+  .menu  li {
+    left: 0px;
+    width: 150px;
+    padding: 0 3px 0 3px;
+    text-align: center;
+   
+  }
+  
+  </style>
+    </head>
+    <body>
+  
+
+  
+</section>
+   
+      </div>  
+     </div>
+ 
+    </div>
+          </div>
+         
+          <div class="col-md-10 col-md-offset-1 " >
+             <div class="col-md-8">
+              <div class="row ">
+                
+
+              <style>
+                  header.carousel {
+                  margin-top: -72px;
+                  margin-left: -243px;
+                  height: 250px;
+                  width: 1305px;
+                }
+
+                  header.carousel .item > img,
+                  header.carousel .item.active  ,
+                  header.carousel .carousel-inner  {
+                  height: 250px; 
+
+                }
+ 
+                  header.carousel .fill > img {
+                  margin-top: 1px;
+                  width: 1200px;
+                  height: 250px; 
+                  margin-left: 60px;
+                  /*margin-left: 9%;*/
+  
+                }
+                @media only screen and (max-width: 768px) {
+                   header.carousel {
+                    margin-top: -40px;
+                    width: 424px;
+                    height: 100%;
+                    margin-left: -40px;
+                   }
+
+                   header.carousel .item > img,
+                   header.carousel .item.active,
+                   header.carousel .carousel-inner {
+                   height: 119px;
+                   }
+
+                   header.carousel .fill > img {
+                    margin-top: 0px;
+                    width: 80%;
+                    height: 123px;
+                    margin-left: 40px;
+                  }
+                  }
+                </style>
+
+                <header id="myCarousel" class="carousel slide"  >
+                 
+           
+                  <div class="carousel-inner">
+                      <div class="item active">
+                      <div class="fill">  
+                              <img  src="img/ans1.png"/> 
+                        </div>
+                          <div class="carousel-caption">
+                              <!-- <h2>K12 Now Ready</h2> -->
+                          </div>
+                      </div>
+                      <div class="item">
+                      <div class="fill">  
+                              <img  src="img/ans2.png"   /> 
+                        </div>
+                          <div class="carousel-caption">
+                              <!-- <h2>K12 Now Ready</h2> -->
+                          </div>
+                      </div>
+                      <div class="item">
+                      <div class="fill" >  
+                              <img  src="img/ans3.png"   /> 
+                        </div>
+                          <div class="carousel-caption">
+                              <!-- <h2>K12 Now Ready</h2> -->
+                          </div>
+                      </div>
+                     
+                  </div>
+          
+                   <!-- Controls -->
+                  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                      <span class="icon-prev"></span>
+                  </a>
+                  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                      <span class="icon-next"></span>
+                  </a>
+          
+          </header>
+           
+          <header2>
+            <!--green bar-->
+           </header2>
+
+
+
+              </div>  
+             </div>
+         
+            </div>
+     
+
+      
+
+  </div>
+
+  <!--Features-->
+<section id="feature" class="fading-background">
+           
+  <div class="title-text" >
+
+  <h11 class="hf">Anselmo A. Sandoval Memorial National High School</h11>
+   
+  </div>
+  
+  <div class="feature-box">
+ 
+      <div class="features">
+          <h11>AASMNHS</h11>
+          <div class="features-desc">
+            <div class="feature-icon">
+              <i class="fa fa-cutlery" aria-hidden="true"></i>
+            </div>
+            <div class="feature-text">
+              <p>There have been several claims for the longest sentence in the English language revolving around the longest printed sentence.
+
+                Sentences can be made arbitrarily long in various ways. One method is successive iterations, such as "Someone thinks that someone thinks that someone thinks that someone thinks that...,"[1] or by combining shorter clauses. Sentences can also be extended by recursively embedding clauses one into another, such as 
+                  </p>
+            </div>
+          </div>
+          <h11>AASMNHS</h11>
+          <div class="features-desc">
+            <div class="feature-icon">
+              <i class="fa fa-smile-o" aria-hidden="true"></i>
+            </div>
+            <div class="feature-text">
+              <p>There have been several claims for the longest sentence in the English language revolving around the longest printed sentence.
+
+                Sentences can be made arbitrarily long in various ways. One method is successive iterations, such as "Someone thinks that someone thinks that someone thinks that someone thinks that...,"[1] or by combining shorter clauses. Sentences can also be extended by recursively embedding clauses one into another, such as 
+                  </p>
+            </div>
+          </div>
+          <h11>AASMNHS</h11>
+          <div class="features-desc">
+            <div class="feature-icon">
+              <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+            </div>
+            <div class="feature-text">
+              <p>There have been several claims for the longest sentence in the English language revolving around the longest printed sentence.
+
+                Sentences can be made arbitrarily long in various ways. One method is successive iterations, such as "Someone thinks that someone thinks that someone thinks that someone thinks that...,"[1] or by combining shorter clauses. Sentences can also be extended by recursively embedding clauses one into another, such as
+                  </p>
+            </div>
+          </div>
+      </div>
+      <div class="features-img">
+          <img src="img/logonbg.png">
+      </div>
+  </div>
+ 
+</section>
+<!--Features End-->
+
+<!--Service -->
+<section id="service">
+<div class="title-text">
+  <p>DepEd</p>
+  <h11>AASMNHS</h11>
+</div>
+<div class="service-box">
+  <div class="single-service">
+      <img src="img/ans1.png">
+      <div class="overlays"></div>
+      <div class="service-desc">
+          <h3>1</h3>
+          <hr>
+          <p>There have been several claims for the longest sentence in the English language revolving around the longest printed sentence.</p>
+      </div>
+  </div>
+  <div class="single-service">
+      <img src="img/ans2.png">
+      <div class="overlays"></div>
+      <div class="service-desc">
+          <h3>2</h3>
+          <hr>
+          <p>There have been several claims for the longest sentence in the English language revolving around the longest printed sentence.</p>
+      </div>
+  </div>
+  <div class="single-service">
+      <img src="img/ans3.png">
+      <div class="overlays"></div>
+      <div class="service-desc">
+          <h3>3</h3>
+          <hr>
+          <p>There have been several claims for the longest sentence in the English language revolving around the longest printed sentence.</p>
+      </div>
+  </div>
+  <div class="single-service"> 
+       <img src="img/ans1.png">
+       <div class="overlays"></div>
+       <div class="service-desc">
+          <h3>4</h3>
+          <hr>
+          <p>There have been several claims for the longest sentence in the English language revolving around the longest printed sentence.</p>
+      </div>
+  </div>
+</div>
+
+</section>
+<!--Service end-->
+      
+
+  <!--Contact -->
+  <section id="contact">
+    <div class="title-text">
+        <p>AASMNHS CONTACT</p>
+    </div>
+   
+
+  </section>
+<!--Contact end-->
+
+<!--footer-->
+<section id="footer">
+  <img src="img/logonbg.png" class="footer-img">
+  <div class="title-text">
+      <p style="color: black; font-size: 4vh;">Anselmo A. Sandoval Memorial National High School Enrollment System</p>
+      
+  
+  </div>
+  <div class="footer-row">
+      <div class="footer-left">
+          <h11>We Are Open In</h11> 
+          <p><i class="fa fa-clock-o"></i> Monday to Friday - 7am to 5pm</p>
+          
+      </div>
+      <div class="footer-right">
+          <h11>Contact</h11> 
+          <p>Brgy. Pulong Niogan, Mabini Batangas <i class="fa fa-map-marker"></i></p>
+          <p>https://www.facebook.com/aasmnhs1999official/<i class="fa fa-paper-plane"></i></p>
+          <p>09467554999<i class="fa fa-phone"></i></p>
+      </div>
+  </div>
+  <div class="social-links">
+      <i class="fa fa-facebook"></i>
+      <i class="fa fa-instagram"></i>
+      <i class="fa fa-twitter"></i>
+      <i class="fa fa-youtube-play"></i>
+      <p>Anselmo A. Sandoval Memorial National High School<br>2023-2024 Enrollment System</p>
+  </div>
+  
+</section>
+<!--footer end-->     
+
+
+
+
+
+
+        
+
+<footer class="panel-footer" style=" margin-left: -15px;
+margin-right: 1px;
+background-color:#098744;color:#fff;">
+            <p align="left" style="margin-left: 20px;" >&copy; Anselmo A. Sandoval Memorial National High School</p>
+         </footer>
+    </div>
+
+</div>  
+
+
+ 
